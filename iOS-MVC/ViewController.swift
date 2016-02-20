eca99afa4aa526d8d6c6c6fa933a1120be2b9d65
Ciprian Cojan <cipry_10@live.it>
2016-02-20 12:12:11 +0100

    Initial Commit

---
+++ iOS-MVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-MVC
+//
+//  Created by Ciprian Cojan on 20/02/16.
+//  Copyright © 2016 Ciprian Cojan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
